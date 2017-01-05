63b5a784817449ca93a216f30ff72ccc1b460e98
Alexey Merezhin <amerezhin@e3ag.ch>
2017-01-05 18:17:54 +0100

    change param list to explicit params

--- src/main/java/org/jvnet/hudson/plugins/m2release/pipeline/M2ReleaseStep.java
+++ src/main/java/org/jvnet/hudson/plugins/m2release/pipeline/M2ReleaseStep.java
@@ -2,7 +2,6 @@ package org.jvnet.hudson.plugins.m2release.pipeline;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.annotation.Nonnull;
@@ -10,13 +9,9 @@ import javax.annotation.Nonnull;
 import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
 import org.jenkinsci.plugins.workflow.steps.AbstractStepExecutionImpl;
 import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl;
-import org.jenkinsci.plugins.workflow.steps.Step;
-import org.jenkinsci.plugins.workflow.steps.StepContext;
 import org.jenkinsci.plugins.workflow.steps.StepContextParameter;
 import org.jenkinsci.plugins.workflow.util.StaplerReferer;
 import org.jvnet.hudson.plugins.m2release.M2ReleaseArgumentsAction;
-import org.jvnet.hudson.plugins.m2release.M2ReleaseBadgeAction;
-import org.jvnet.hudson.plugins.m2release.M2ReleaseBuildWrapper;
 import org.jvnet.hudson.plugins.m2release.ReleaseCause;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.DoNotUse;
@@ -24,7 +19,6 @@ import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 import org.kohsuke.stapler.QueryParameter;
-import org.kohsuke.stapler.StaplerRequest;
 
 import com.google.inject.Inject;
 
@@ -34,25 +28,18 @@ import hudson.console.ModelHyperlinkNote;
 import hudson.model.AbstractProject;
 import hudson.model.Action;
 import hudson.model.AutoCompletionCandidates;
-import hudson.model.BooleanParameterDefinition;
-import hudson.model.BuildableItemWithBuildWrappers;
 import hudson.model.Cause;
 import hudson.model.CauseAction;
-import hudson.model.Descriptor;
 import hudson.model.Hudson;
 import hudson.model.ItemGroup;
 import hudson.model.Job;
-import hudson.model.ParameterDefinition;
 import hudson.model.ParameterValue;
 import hudson.model.ParametersAction;
 import hudson.model.Run;
-import hudson.model.StringParameterDefinition;
 import hudson.model.TaskListener;
 import hudson.model.queue.QueueTaskFuture;
 import jenkins.model.Jenkins;
 import jenkins.model.ParameterizedJobMixIn;
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
 
 /**
  * Created by e3cmea on 1/3/17.
@@ -63,12 +50,13 @@ public class M2ReleaseStep extends AbstractStepImpl {
     private static final Logger LOGGER = Logger.getLogger(M2ReleaseStep.class.getName());
 
     private String job;
-    private List<ParameterValue> parameters;
+    private String releaseVersion = "";
+    private String developmentVersion = "";
+    private Boolean isDryRun = false;
 
     @DataBoundConstructor
     public M2ReleaseStep(String job) {
         this.job = job;
-        parameters = new ArrayList<>();
     }
 
     public String getJob() {
@@ -79,12 +67,28 @@ public class M2ReleaseStep extends AbstractStepImpl {
         this.job = job;
     }
 
-    public List<ParameterValue> getParameters() {
-        return parameters;
+    public String getReleaseVersion() {
+        return releaseVersion;
     }
 
-    @DataBoundSetter public void setParameters(List<ParameterValue> parameters) {
-        this.parameters = parameters;
+    @DataBoundSetter public void setReleaseVersion(String releaseVersion) {
+        this.releaseVersion = releaseVersion;
+    }
+
+    public String getDevelopmentVersion() {
+        return developmentVersion;
+    }
+
+    @DataBoundSetter public void setDevelopmentVersion(String developmentVersion) {
+        this.developmentVersion = developmentVersion;
+    }
+
+    public Boolean isDryRun() {
+        return isDryRun;
+    }
+
+    @DataBoundSetter public void setDryRun(Boolean dryRun) {
+        isDryRun = dryRun;
     }
 
     public static class Execution extends AbstractStepExecutionImpl {
@@ -92,35 +96,6 @@ public class M2ReleaseStep extends AbstractStepImpl {
         @StepContextParameter private transient TaskListener listener;
 
         @Inject(optional=true) transient M2ReleaseStep step;
-
-        private List<ParameterValue> updateParametersWithDefaults(AbstractProject project,
-                List<ParameterValue> parameters) throws AbortException {
-
-            if (project instanceof BuildableItemWithBuildWrappers) {
-                M2ReleaseBuildWrapper wrapper = ((BuildableItemWithBuildWrappers) project).getBuildWrappersList()
-                                                                                   .get(M2ReleaseBuildWrapper.class);
-                if (wrapper != null) {
-                    /* TODO
-                    for (ParameterDefinition pd : wrapper.getParameterDefinitions()) {
-                        boolean parameterExists = false;
-                        for (ParameterValue pv : parameters) {
-                            if (pv.getName()
-                                  .equals(pd.getName())) {
-                                parameterExists = true;
-                                break;
-                            }
-                        }
-                        if (!parameterExists) {
-                            parameters.add(pd.getDefaultParameterValue());
-                        }
-                    }
-                    */
-                } else {
-                    throw new AbortException("Job doesn't have release plugin configuration");
-                }
-            }
-            return parameters;
-        }
 
         @Override
         public boolean start() throws Exception {
@@ -153,19 +128,18 @@ public class M2ReleaseStep extends AbstractStepImpl {
             ParametersAction parameters = new ParametersAction(values);
 
             M2ReleaseArgumentsAction arguments = new M2ReleaseArgumentsAction();
-            arguments.setDryRun(isDryRun);
+            arguments.setDryRun(step.isDryRun());
+            arguments.setReleaseVersion(step.getReleaseVersion());
+            arguments.setDevelopmentVersion(step.getDevelopmentVersion());
 
-            arguments.setReleaseVersion(releaseVersion);
-            arguments.setDevelopmentVersion(developmentVersion);
-
-            arguments.setCloseNexusStage(closeNexusStage);
-            arguments.setRepoDescription(repoDescription);
-            arguments.setScmUsername(scmUsername);
-            arguments.setScmPassword(scmPassword);
-            arguments.setScmTagName(scmTag);
-            arguments.setScmCommentPrefix(scmCommentPrefix);
-            arguments.setAppendHusonUserName(appendHusonUserName);
-            arguments.setHudsonUserName(Hudson.getAuthentication().getName());
+            // arguments.setCloseNexusStage(closeNexusStage);
+            // arguments.setRepoDescription(repoDescription);
+            // arguments.setScmUsername(scmUsername);
+            // arguments.setScmPassword(scmPassword);
+            // arguments.setScmTagName(scmTag);
+            // arguments.setScmCommentPrefix(scmCommentPrefix);
+            // arguments.setAppendHusonUserName(appendHusonUserName);
+            // arguments.setHudsonUserName(Hudson.getAuthentication().getName());
 
             List<Action> actions = new ArrayList<>();
             actions.add(new M2ReleaseTriggerAction(getContext()));
@@ -203,48 +177,6 @@ public class M2ReleaseStep extends AbstractStepImpl {
         @Override
         public String getDisplayName() {
             return "Trigger M2 release for the job";
-        }
-
-        @Override public Step newInstance(StaplerRequest req, JSONObject formData) throws Descriptor.FormException {
-            M2ReleaseStep step = (M2ReleaseStep) super.newInstance(req, formData);
-            // Cf. ParametersDefinitionProperty._doBuild:
-            Object parameter = formData.get("parameter");
-            JSONArray params = parameter != null ? JSONArray.fromObject(parameter) : null;
-            if (params != null) {
-                Jenkins jenkins = Jenkins.getInstance();
-                Job<?,?> context = StaplerReferer.findItemFromRequest(Job.class);
-                Job<?,?> job = jenkins != null ? jenkins.getItem(step.getJob(), context, Job.class) : null;
-
-                AbstractProject project = Jenkins.getActiveInstance()
-                                                       .getItem(step.getJob(), context, AbstractProject.class);
-
-                if (project instanceof BuildableItemWithBuildWrappers) {
-                    M2ReleaseBuildWrapper wrapper = ((BuildableItemWithBuildWrappers) project).getBuildWrappersList()
-                                                                                                             .get(M2ReleaseBuildWrapper.class);
-                    if (wrapper == null) {
-                        throw new IllegalArgumentException("Job doesn't have release plugin configuration");
-                    }
-
-                        List<ParameterValue> values = new ArrayList<>();
-                        for (Object o : params) {
-                            JSONObject jo = (JSONObject) o;
-                            String name = jo.getString("name");
-                            LOGGER.info(jo.toString());
-                            ParameterDefinition pd;
-                            if ("MVN_ISDRYRUN".equals(name)) {
-                                pd = new BooleanParameterDefinition(name, false, "isDryRun");
-                            } else {
-                                pd = new StringParameterDefinition(name, "");
-                            }
-                            ParameterValue parameterValue = pd.createValue(req, jo);
-                            values.add(parameterValue);
-                        }
-                        step.setParameters(values);
-                } else {
-                    throw new IllegalArgumentException("Wrong job type: " + project.getClass().getName());
-                }
-            }
-            return step;
         }
 
         public AutoCompletionCandidates doAutoCompleteJob(@AncestorInPath ItemGroup<?> context, @QueryParameter String value) {
