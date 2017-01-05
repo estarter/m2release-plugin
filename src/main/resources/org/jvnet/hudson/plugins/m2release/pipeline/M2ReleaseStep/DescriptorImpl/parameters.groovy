9d5a441d1e9063ac0963abe84f2fb32b4ba96c29
Alexey Merezhin <amerezhin@e3ag.ch>
2017-01-05 17:17:26 +0100

    fix pipeline generation

--- src/main/resources/org/jvnet/hudson/plugins/m2release/pipeline/M2ReleaseStep/DescriptorImpl/parameters.groovy
+++ src/main/resources/org/jvnet/hudson/plugins/m2release/pipeline/M2ReleaseStep/DescriptorImpl/parameters.groovy
@@ -1,9 +1,12 @@
 package org.jvnet.hudson.plugins.m2release.pipeline.M2ReleaseStep.DescriptorImpl
 
 import hudson.model.AbstractProject
+import hudson.model.BooleanParameterDefinition
 import hudson.model.BuildableItemWithBuildWrappers
 import hudson.model.StringParameterDefinition
-import hudson.plugins.release.ReleaseWrapper;
+import org.jvnet.hudson.plugins.m2release.M2ReleaseAction
+import org.jvnet.hudson.plugins.m2release.M2ReleaseBuildWrapper
+
 def st = namespace('jelly:stapler')
 def l = namespace('/lib/layout')
 l.ajax {
@@ -15,24 +18,24 @@ l.ajax {
 
         if (project != null) {
             if (project instanceof BuildableItemWithBuildWrappers) {
-                def wrapper = project.getBuildWrappersList().get(ReleaseWrapper.class)
+                def wrapper = project.getBuildWrappersList().get(M2ReleaseBuildWrapper.class)
 
                 if (wrapper != null) {
-                    def parameterDefinitionList = wrapper.getParameterDefinitions()
-                    if (parameterDefinitionList.isEmpty()) {
-                        parameterDefinitionList.add(new StringParameterDefinition("RELEASE_VERSION", "", "Release version"))
-                        parameterDefinitionList.add(new StringParameterDefinition("DEVELOPMENT_VERSION", "", "Next development version"))
-                    }
+                    def action = new M2ReleaseAction(project, false, false, false)
+                    def values = []
+                    values.add(new StringParameterDefinition(M2ReleaseBuildWrapper.DescriptorImpl.DEFAULT_RELEASE_VERSION_ENVVAR, action.computeReleaseVersion(), ""))
+                    values.add(new StringParameterDefinition(M2ReleaseBuildWrapper.DescriptorImpl.DEFAULT_DEV_VERSION_ENVVAR, action.computeNextVersion(), ""))
+                    values.add(new BooleanParameterDefinition(M2ReleaseBuildWrapper.DescriptorImpl.DEFAULT_DRYRUN_ENVVAR, false, ""))
 
                     table(width: '100%', class: 'parameters') {
-                        for (parameterDefinition in parameterDefinitionList) {
+                        for (parameterDefinition in values) {
                             tbody {
                                 st.include(it: parameterDefinition, page: parameterDefinition.descriptor.valuePage)
                             }
                         }
                     }
                 } else {
-                    text("${project.fullDisplayName} doesn't have release plugin configuration")
+                    text("${project.fullDisplayName} doesn't have m2 release plugin configuration")
                 }
             } else {
                 text("${project.fullDisplayName} is of wrong type and can't be released: ${project.class.simpleName}")
